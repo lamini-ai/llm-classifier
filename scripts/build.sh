871472da81559658862f030cb3d2675c1654ccd6
Shreya Gupta <40699385+ShreyaGupta08@users.noreply.github.com>
2023-10-09 16:46:56 -0700

    Update dir name build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,5 +13,5 @@ set -Eeuoxa pipefail
 # Get the directory of this script
 LOCAL_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t laminify:latest $LOCAL_DIRECTORY/..
+docker build -t lamini_classifier:latest $LOCAL_DIRECTORY/..
 
