a24ffe7b39d265921433d04050bf293bd066e3c3
Sharon Zhou <sharon@lamini.ai>
2023-10-13 14:34:52 -0700

    submits both locally and to hosted service

--- host/submit.sh
+++ host/submit.sh
@@ -2,22 +2,22 @@
 
 if [ "$#" -eq 1 ]; then
     host_url=$1
+    endpoint=":5000/predict" # no trailing slash
 else
     host_url="http://127.0.0.1"
+    endpoint=":5000/predict/"
 fi
 
-endpoint=":5000/predict/"
-
-url="$host_url$endpoint"
-
+url=$host_url$endpoint
 echo "Submitting to $url"
 
 curl -X POST -H "Content-Type: multipart/form-data" \
-     -F "model=@models/model.lamini" \
-     -F "data={\"data\": \"woof\"}" \
-     "$url"
+    -F "model=@models/model.lamini" \
+    -F "data={\"data\": \"woof\"}" \
+    "$url"
 
 curl -X POST -H "Content-Type: multipart/form-data" \
-     -F "model=@models/model.lamini" \
-     -F "data={\"data\": [\"woof\", \"meow\"]}" \
-     "$url"
+    -H "Authorization: Bearer test_token" \
+    -F "model=@models/model.lamini" \
+    -F "data={\"data\": [\"woof\", \"meow\"]}" \
+    "$url"
