0a4600eaeba0809d37d09032fe1d72f2113d40cf
Sharon Zhou <sharon@lamini.ai>
2023-10-13 14:01:41 -0700

    submit curl

--- host/submit.sh
+++ host/submit.sh
@@ -1,1 +1,23 @@
-curl -X POST -H "Content-Type: multipart/form-data" -F "model=@models/model.lamini" -F "data={\"data\": \"woof\"}" http://127.0.0.1:5000/predict/
+#!/bin/bash
+
+if [ "$#" -eq 1 ]; then
+    host_url=$1
+else
+    host_url="http://127.0.0.1"
+fi
+
+endpoint=":5000/predict/"
+
+url="$host_url$endpoint"
+
+echo "Submitting to $url"
+
+curl -X POST -H "Content-Type: multipart/form-data" \
+     -F "model=@models/model.lamini" \
+     -F "data={\"data\": \"woof\"}" \
+     "$url"
+
+curl -X POST -H "Content-Type: multipart/form-data" \
+     -F "model=@models/model.lamini" \
+     -F "data={\"data\": [\"woof\", \"meow\"]}" \
+     "$url"
